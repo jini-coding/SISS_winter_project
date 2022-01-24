2ba3112c6e59ae88628540d27b77002591195a9b
chosolbee <96732525+chosolbee@users.noreply.github.com>
2022-01-24 12:32:26 +0900

    Update ott_index.php

--- ott_index.php
+++ ott_index.php
@@ -8,6 +8,8 @@
         padding: 35px;
         color :#A50000;
         border-bottom:1px solid black;
+        font-size: 5em;
+        font-family : sans-serif;
       }
       img{
         box-shadow : 5px 5px 5px #BDBDBD;
@@ -15,15 +17,63 @@
       }
       body{
         background-color:#FFD8D8;
+        height : 2000px;
+      }
+
+      nav, section{
+        display : flex;
+        justify-content: center;
+      }
+      nav a{
+        text-decoration: none;
+        color : black;
+        margin : 2em;
+      }
+      #underline{
+        position : absolute;
+        width:0;
+        background-color: black;
+        top:340px;
+        left:0;
+        height:4px;
+        transition:0.5s;
       }
     </style>
     <title></title>
   </head>
   <body>
     <h1>OTT별 콘텐츠 추천</h1>
-      <a href="index.php?id=NETFLIX"><img src="netflix_img.PNG"></a>
-      <a href="index.php?id=TVING"><img src="tving_img.PNG"></a>
-      <a href="index.php?id=WATCHA"><img src="watcha_img.PNG"></a>
-      <a href="index.php?id=Disney+"><img src="disney+_img.PNG"></a>
+    <nav>
+      <div id="underline"></div>
+      <a href="#">About</a>
+      <a href="#">Board</a>
+      <a href="#">Search</a>
+    </nav>
+    <script>
+      let under = document.getElementById("underline");
+      let menu = document.querySelectorAll("nav a");
+
+      menu.forEach((menu)=>
+        menu.addEventListener("click",(e)=>indicator(e))
+      );
+      function indicator(e){
+        under.style.left=e.currentTarget.offsetLeft+"px";
+        under.style.width=e.currentTarget.offsetWidth+"px";
+        under.style.top=
+           e.currentTarget.offsetTop+e.currentTarget.offsetheight+"px";
+      }
+      menu.forEach((menu)=>
+        menu.addEventListener("mouseover",(e) =>
+          indicator(e.currentTarget)
+        )
+      );
+    </script>
+    <section>
+      <a href="netflix.html"><img class="netflix_icon" src="netflix_img.PNG"></a>
+      <a href="index.php?id=TVING"><img class="tving_icon" src="tving_img.PNG"></a>
+      <a href="index.php?id=WATCHA"><img class="watcha_icon" src="watcha_img.PNG"></a>
+      <a href="index.php?id=Disney"><img class="disney_icon" src="disney+_img.PNG"></a>
+  </section>
+
   </body>
 </html>
